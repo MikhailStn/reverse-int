eccd201f1dad0cfa6f4fbc2329a565561910ab5f
Mikhail Stankevich <mikhailstn1@gmail.com>
2022-01-19 07:29:26 +0300

    feat: add code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function reverse (n) {
-  
-}
+    let str = n.toString();
+
+    if (str[0] === '-') {
+        let positiveString = str.substring(1);
+        let newStr = positiveString.split("").reverse().join("");
+        let result = Number(newStr);
+        return result
+    } else {
+        let newStr = str.split("").reverse().join("");
+        let result = Number(newStr);
+        return result;
+    }
+};
